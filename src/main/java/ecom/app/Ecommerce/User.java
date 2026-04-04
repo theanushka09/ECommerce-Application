572ee7e49fa11837e37dbf1be0f2bd5adf908ea6
Anushka Singh <82802735+theanushka09@users.noreply.github.com>
2026-04-05 00:24:00 +0530

    Update User.java

--- src/main/java/ecom/app/Ecommerce/User.java
+++ src/main/java/ecom/app/Ecommerce/User.java
@@ -1,9 +1,19 @@
 package ecom.app.Ecommerce;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@Entity
+//@Entity(name="user_table")
 public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
