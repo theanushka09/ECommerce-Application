0f516eacb4a3aab9d7a3358cfa9edf3a1d95ede8
Anushka Singh <82802735+theanushka09@users.noreply.github.com>
2026-04-05 00:22:47 +0530

    Update UserController.java

--- src/main/java/ecom/app/Ecommerce/UserController.java
+++ src/main/java/ecom/app/Ecommerce/UserController.java
@@ -7,29 +7,30 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;  //creating ref of UserService
 
-    @GetMapping("/api/users")
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.fetchAllUsers());
     }
 
-    @PostMapping("/api/users")
+    @PostMapping
     public ResponseEntity<String> createUser(@RequestBody User user) {
         return ResponseEntity.ok(userService.addUser(user));
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/api/users/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateUser(@PathVariable Long id , @RequestBody User updatedUser) {
         boolean updated = userService.updateUser(id, updatedUser);
         if(updated) {
