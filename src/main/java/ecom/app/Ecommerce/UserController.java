f8e4136a785db4f8d5367c05e28baa56eb96f7c4
Anushka Singh <82802735+theanushka09@users.noreply.github.com>
2026-04-05 04:24:50 +0530

    Update UserController.java
    mapping request and response dtos

--- src/main/java/ecom/app/Ecommerce/UserController.java
+++ src/main/java/ecom/app/Ecommerce/UserController.java
@@ -1,5 +1,8 @@
 package ecom.app.Ecommerce;
 
+import ecom.app.Ecommerce.DTO.UserRequest;
+import ecom.app.Ecommerce.DTO.UserResponse;
+import ecom.app.Ecommerce.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,25 +17,26 @@ public class UserController {
     private UserService userService;  //creating ref of UserService
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
         return ResponseEntity.ok(userService.fetchAllUsers());
     }
 
     @PostMapping
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-        return ResponseEntity.ok(userService.addUser(user));
+    public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest) {
+        userService.addUser(userRequest);
+        return ResponseEntity.ok("user added successfully");
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateUser(@PathVariable Long id , @RequestBody User updatedUser) {
-        boolean updated = userService.updateUser(id, updatedUser);
+    public ResponseEntity<String> updateUser(@PathVariable Long id , @RequestBody UserRequest updatedUserRequest) {
+        boolean updated = userService.updateUser(id, updatedUserRequest);
         if(updated) {
             return ResponseEntity.ok("User updated successfully");
         }
